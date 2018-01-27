2c1286d80a6f17587d7f76bb1bca8420c62d2d09
Nicolás M. Pardo <nicolasm@nikodermus.media>
2018-01-27 15:17:04 -0500

    👶🏽   Create yours

---
+++ 01 structure/index.js
@@ -0,0 +1,21 @@
+const person = {
+  name: 'Nico',
+  age: '21',
+};
+
+const handler = {
+  get(target, key) { //Short to 'get: function(){}'
+    if (key === 'name') {
+      return `My name is ${target[key]}`;
+    }
+    return target[key];
+  },
+
+  set(target, key, value) {
+    target[key] = value.trim();
+  },
+};
+
+const proxy_person = new Proxy(person, handler);
+
+console.log(proxy_person.name);
